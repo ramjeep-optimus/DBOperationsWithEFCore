ff9ecfc22d2ecdcba91b65139e73a65b44fc2148
ramjeep-optimus <ramjee.pal@optimusinfo.com>
2025-04-09 16:34:54 +0530

    First Commit

---
+++ DBOperationsWithEFCore/Migrations/20250401095753_LanguageTableAdded.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBOperationsWithEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class LanguageTableAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "LanguageId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Language", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_LanguageId",
+                table: "Books",
+                column: "LanguageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Language_LanguageId",
+                table: "Books",
+                column: "LanguageId",
+                principalTable: "Language",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Language_LanguageId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Language");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_LanguageId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageId",
+                table: "Books");
+        }
+    }
+}
---
+++ DBOperationsWithEFCore/Migrations/20250402044919_updatedType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBOperationsWithEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[] { 1, "Indian INR", "INR" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
